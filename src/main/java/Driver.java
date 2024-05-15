8d3d2418caebb0dc7ae89b5ac47dee796f45192a
Law1911 <126835746+Law1911@users.noreply.github.com>
2024-05-15 20:21:45 +0700

    Update Driver.java
    Mengubah driver pada print supaya menjadi lebih jelas

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,15 +6,15 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Count: Jumlahnya adalah "+counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Count: Jumlah setelah di tambahkan" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Count: Jumlah setelah di kurang" + counter.getCount());
 		
 	}
 
